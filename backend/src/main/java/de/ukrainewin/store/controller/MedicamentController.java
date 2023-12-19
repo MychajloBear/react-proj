fb6c574e2a2f1f2036087c920df8e9fcfbf1df87
Michajlo <vedmidmetal@ukr.net>
2023-12-19 10:30:31 +0100

    add elastic client initialization

--- backend/src/main/java/de/ukrainewin/store/controller/MedicamentController.java
+++ backend/src/main/java/de/ukrainewin/store/controller/MedicamentController.java
@@ -4,12 +4,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+//import org.elasticsearch.client.internal.ElasticsearchClient;
+import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.apache.http.HttpHost;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 
 @RestController
 @RequestMapping("api/med")
@@ -21,6 +29,13 @@ public class MedicamentController {
 	@GetMapping("{id}")
 	    public List</*Food*/String> listSuggestedMedicamentsId(@PathVariable String id) {
 	        System.out.println("listSuggestedMedicamentsId");
+
+			RestClient restClient = RestClient
+					.builder(HttpHost.create("http://localhost:9200"))
+					.build();
+			ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+			ElasticsearchClient client = new ElasticsearchClient(transport);
+
 	        return List.of("id med", "exists");//foodService.list();
 	    }
 	
