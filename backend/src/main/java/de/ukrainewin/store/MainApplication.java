cc2bbc32cf5a37524c77983b507e71dfdef56760
Michajlo <vedmidmetal@ukr.net>
2023-12-20 10:40:10 +0100

    code format

--- backend/src/main/java/de/ukrainewin/store/MainApplication.java
+++ backend/src/main/java/de/ukrainewin/store/MainApplication.java
@@ -2,8 +2,6 @@ package de.ukrainewin.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
@@ -11,7 +9,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
 @SpringBootApplication
-@OpenAPIDefinition(
+	@OpenAPIDefinition(
 		info = @Info(
 				title = "Test microservice REST API Documentation",
 				description = "Just blank dummy microservice REST API Documentation",
